95bfbddc8999e0ca55afce53295cc9f013e5c63e
Thomas la Cour <tlc@hyrtwol.dk>
2024-10-14 18:07:10 +0200

    misc experiments

---
+++ examples/setup/build.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal enabledelayedexpansion
+if "%VSCMD_ARG_TGT_ARCH%"=="" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+pushd %~dp0..\..
+@echo on
+odin run examples/setup -resource:examples/setup/setup.rc -vet -- %*
+@echo off
+popd
