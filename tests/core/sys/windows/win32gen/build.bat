f4a424914f384923cb33b9478e27631d5c101d41
Thomas la Cour <tlc@hyrtwol.dk>
2026-07-20 17:18:37 +0200

    window styles cleanup

--- tests/core/sys/windows/win32gen/build.bat
+++ tests/core/sys/windows/win32gen/build.bat
@@ -1,5 +1,24 @@
 @echo off
-if "%VSCMD_ARG_TGT_ARCH%"=="" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+setlocal EnableDelayedExpansion
+
+where /Q cl.exe || (
+	set __VSCMD_ARG_NO_LOGO=1
+	for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath') do set VS=%%i
+	if "!VS!" equ "" (
+		echo ERROR: MSVC installation not found
+		exit /b 1
+	)
+	call "!VS!\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64 || exit /b 1
+)
+
+if "%VSCMD_ARG_TGT_ARCH%" neq "x64" (
+	if "%ODIN_IGNORE_MSVC_CHECK%" == "" (
+		echo ERROR: please run this from MSVC x64 native tools command prompt, 32-bit target is not supported!
+		exit /b 1
+	)
+)
+
 set genconfig=Release
 pushd %~dp0
 
